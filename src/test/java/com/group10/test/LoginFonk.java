6f883c4f2a11ec7fc5c94ae3d36d4bf6ee90b986
ozturkkerem <ozturk.kerem@live.com>
2022-02-16 11:00:06 -0500

    login test is done

---
+++ src/test/java/com/group10/test/LoginFonk.java
@@ -0,0 +1,12 @@
+package com.group10.test;
+
+import org.testng.annotations.Test;
+
+public class LoginFonk {
+
+    @Test
+    public void login(){
+        System.out.println("Be patient");
+
+    }
+}
