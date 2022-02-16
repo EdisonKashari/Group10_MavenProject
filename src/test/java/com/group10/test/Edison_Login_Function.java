e1f6fc5adfc28306179bdda25135d81b4c1528b3
Edison Kashari <edisonkashari@hotmail.com>
2022-02-16 11:07:53 -0500

    first logout step

---
+++ src/test/java/com/group10/test/Edison_Login_Function.java
@@ -0,0 +1,10 @@
+package com.group10.test;
+
+import org.testng.annotations.Test;
+
+public class Edison_Login_Function {
+ @Test
+    public void logout(){
+     System.out.println("First Step");
+ }
+}
